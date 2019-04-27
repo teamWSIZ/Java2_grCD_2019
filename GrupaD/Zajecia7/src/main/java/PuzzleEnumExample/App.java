dfb6c9de3b2a8be239e6deda91d121ed1f85789d
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2019-04-27 09:16:49 +0200

    cd..

--- GrupaD/Zajecia7/src/main/java/PuzzleEnumExample/App.java
+++ GrupaD/Zajecia7/src/main/java/PuzzleEnumExample/App.java
@@ -18,24 +18,24 @@ class PuzzleElement extends ImageView {
     private int mDimension;
 
     @FunctionalInterface
-    public interface onPuzzleClicked{
+    public interface onPuzzleClicked {
         void onClicked(int i, int j);
     }
 
     onPuzzleClicked mListener;
 
-    public void setOnPuzzleClickedListener(onPuzzleClicked listener){
+    public void setOnPuzzleClickedListener(onPuzzleClicked listener) {
         mListener = listener;
     }
 
     WritableImage mImage;
 
-    PuzzleElement(WritableImage image, int dimension, int i, int j){
+    PuzzleElement(WritableImage image, int dimension, int i, int j) {
         super(image);
 
         mDimension = dimension;
 
-        this.setX(i*dimension);
+        this.setX(i * dimension);
 
         mI = i;
         mJ = j;
@@ -50,22 +50,34 @@ public class App extends Application {
     public void start(Stage primaryStage) throws Exception {
         Group group = new Group();
 
-        Scene scene = new Scene(group,500,500);
+        Scene scene = new Scene(group, 500, 500);
 
         createPuzzle();
 
-        group.getChildren().add(mPuzzle[0][0]);
+        for (int j = 0; j < 2; j++)
+            for (int i = 0; i < 2; i++)
+                group.getChildren().add(mPuzzle[i][j]);
 
         primaryStage.setScene(scene);
         primaryStage.show();
     }
 
-    private void createPuzzle(){
+    private void createPuzzle() {
+
         Image image = new Image(getClass().getResource("elephant.png").toString());
 
         PixelReader reader = image.getPixelReader();
-        WritableImage newImage = new WritableImage(reader, 0, 0, (int)image.getWidth()/2, (int)image.getHeight()/2);
 
-        mPuzzle[0][0] = new PuzzleElement(newImage,(int)(image.getWidth()/2),0,0);
+        int d = (int) (image.getWidth() / 2);
+
+        for (int j = 0; j < 2; j++) {
+            for (int i = 0; i < 2; i++) {
+                WritableImage newImage = new WritableImage(reader, i * d, j * d, d, d);
+                mPuzzle[i][j] = new PuzzleElement(newImage, (int) (image.getWidth() / 2), i, j);
+
+                mPuzzle[i][j].setX(i * d);
+                mPuzzle[i][j].setY(j * d);
+            }
+        }
     }
 }
