5e65a70164dfaca4d3005fd2247edb2eaff19ac2
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2019-05-18 08:58:00 +0200

    cd..

--- GrupaD/Zajecia10/src/main/java/com/javafx/App.java
+++ GrupaD/Zajecia10/src/main/java/com/javafx/App.java
@@ -7,7 +7,24 @@ import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
+
+class MyButton extends Button{
+    MyButton(String text){
+        super(text);
+    }
+
+    private void wyswietlAlert(){
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Tytuł okna");
+        alert.setHeaderText("Przykładowy nagłówek");
+        alert.setContentText("Przykładowy tekst");
+
+        alert.showAndWait();
+    }
+}
 
 public class App extends Application {
 
@@ -21,15 +38,26 @@ public class App extends Application {
 
         primaryStage.setScene(scene);
         primaryStage.show();
-        
-    }
 
-    private void wyswietlAlert(){
-        Alert alert = new Alert(Alert.AlertType.INFORMATION);
-        alert.setTitle("Tytuł okna");
-        alert.setHeaderText("Przykładowy nagłówek");
-        alert.setContentText("Przykładowy tekst");
+        MyButton button0 = new MyButton("0");
+        button0.setPrefSize(100,100);
 
-        alert.showAndWait();
+        MyButton button1 = new MyButton("1");
+        button1.setPrefSize(100,100);
+
+        MyButton button2 = new MyButton("2");
+        button2.setPrefSize(100,100);
+
+        MyButton button3 = new MyButton("3");
+        button3.setPrefSize(100,100);
+
+
+        HBox hbox = new HBox();
+
+        button0.setLayoutX(100);
+
+
+        group.getChildren().addAll(button0,button1,button2,button3);
+
     }
 }
