8ab83550f66c8e5cb72bc132f34c4291b894fef6
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2019-05-18 09:26:10 +0200

    cd..

--- GrupaD/Zajecia10/src/main/java/com/javafx/App.java
+++ GrupaD/Zajecia10/src/main/java/com/javafx/App.java
@@ -8,19 +8,31 @@ import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 class MyButton extends Button{
-    MyButton(String text){
-        super(text);
+
+    MyButton(int no){
+        super(""+no);
+
+        setMaxWidth(Double.MAX_VALUE);
+        HBox.setHgrow(this, Priority.ALWAYS);
+
+        setOnMouseClicked(event -> {
+            wyswietlAlert(no);
+        });
     }
 
-    private void wyswietlAlert(){
+    private void wyswietlAlert(int no){
         Alert alert = new Alert(Alert.AlertType.INFORMATION);
         alert.setTitle("Tytuł okna");
         alert.setHeaderText("Przykładowy nagłówek");
-        alert.setContentText("Przykładowy tekst");
+        alert.setContentText(""+no);
 
         alert.showAndWait();
     }
@@ -39,25 +51,22 @@ public class App extends Application {
         primaryStage.setScene(scene);
         primaryStage.show();
 
-        MyButton button0 = new MyButton("0");
-        button0.setPrefSize(100,100);
+        MyButton button0 = new MyButton(0);
+        MyButton button1 = new MyButton(1);
+        MyButton button2 = new MyButton(2);
+        MyButton button3 = new MyButton(100);
 
-        MyButton button1 = new MyButton("1");
-        button1.setPrefSize(100,100);
-
-        MyButton button2 = new MyButton("2");
-        button2.setPrefSize(100,100);
-
-        MyButton button3 = new MyButton("3");
-        button3.setPrefSize(100,100);
+        HBox hbox = new HBox(button0,button1,button2,button3);
+        hbox.setPrefWidth(500);
 
 
-        HBox hbox = new HBox();
 
-        button0.setLayoutX(100);
+        BackgroundFill bf = new BackgroundFill(Color.AZURE,null,null);
+
+        hbox.setBackground(new Background(bf));
 
 
-        group.getChildren().addAll(button0,button1,button2,button3);
+        group.getChildren().add(hbox);
 
     }
 }
