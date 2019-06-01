5a7c56f9811afde2cc9d0ef9f129d2efeb836502
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2019-06-01 13:24:44 +0200

    cd..

--- GrupaD/untitled1/src/Main.java
+++ GrupaD/untitled1/src/Main.java
@@ -1,42 +1,51 @@
 public class Main {
+    @FunctionalInterface
+    private interface wyrazenieLogiczne{
+        boolean wyrazenie(boolean p, boolean q);
+    }
+
 
     public static void main(String[] args) {
 
-        boolean p,q;
-
-        p=false;
-        q=false;
-
-        boolean wyrazenie = !(p||q)==(!p&&!q);
+        System.out.println("----------Koniunkcja-----------");
+        wyswietlTabele(true,true, (p,q)->p&&q);
+        System.out.println("_______________________________");
 
 
-        wyswietlTabele();
+        System.out.println("----------Alternatywa----------");
+        wyswietlTabele(true,true, (p,q)->p||q);
+        System.out.println("_______________________________");
+
+        System.out.println("----------Prawo de Morgana----------");
+        wyswietlTabele(true,true, (p,q)->!(p||q)==(!p&&!q));
+        System.out.println("_______________________________");
+
+
 
 
     }
 
-    static void wyswietlTabele(){
+    static void wyswietlTabele(boolean p, boolean q, wyrazenieLogiczne wyrazenie){
 
         System.out.println("p  "+"  q  "+"  wyrazenie  ");
         System.out.println("_______________________");
 
         System.out.println();
 
-        wyswietlPQ(true,true);
-        wyswietlPQ(true,false);
-        wyswietlPQ(false,true);
-        wyswietlPQ(false,false);
-
+        wyswietlPQ(true,true, wyrazenie.wyrazenie(true,true));
+        wyswietlPQ(true,false, wyrazenie.wyrazenie(true,false));
+        wyswietlPQ(false,true, wyrazenie.wyrazenie(false,true));
+        wyswietlPQ(false,false, wyrazenie.wyrazenie(false,false));
 
     }
 
-    static void wyswietlPQ(boolean p, boolean q){
+    static void wyswietlPQ(boolean p, boolean q, boolean wyrazenie){
         int lp, lq;
 
         if(p)lp = 1; else lp = 0;
         if(q)lq = 1; else lq = 0;
 
-        System.out.println(lp+"    "+lq);
+        System.out.println(lp+"    "+lq+"    "+wyrazenie);
     }
 
     static void wyswietl(String napis){
