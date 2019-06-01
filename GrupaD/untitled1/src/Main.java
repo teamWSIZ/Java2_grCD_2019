1e77268f480a8cf6a793355658199ffc7e376bbb
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2019-06-01 13:08:05 +0200

    cd..

--- GrupaD/untitled1/src/Main.java
+++ GrupaD/untitled1/src/Main.java
@@ -10,13 +10,33 @@ public class Main {
         boolean wyrazenie = !(p||q)==(!p&&!q);
 
 
-        if(wyrazenie){
-            wyswietl("Wyrażenie jest prawdziwe");
-        }else{
-            wyswietl("Nie jest prawdziwe");
-        }
+        wyswietlTabele();
 
 
+    }
+
+    static void wyswietlTabele(){
+
+        System.out.println("p  "+"  q  "+"  wyrazenie  ");
+        System.out.println("_______________________");
+
+        System.out.println();
+
+        wyswietlPQ(true,true);
+        wyswietlPQ(true,false);
+        wyswietlPQ(false,true);
+        wyswietlPQ(false,false);
+
+
+    }
+
+    static void wyswietlPQ(boolean p, boolean q){
+        int lp, lq;
+
+        if(p)lp = 1; else lp = 0;
+        if(q)lq = 1; else lq = 0;
+
+        System.out.println(lp+"    "+lq);
     }
 
     static void wyswietl(String napis){
