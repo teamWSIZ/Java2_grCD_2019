e9e1a684ccb1d3fd06c59a555b8ddac3f0b15d77
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2019-06-08 09:38:16 +0200

    cd..

---
+++ GrupaD/Zajecia13/src/main/java/com/junit/FiguryGeometryczne.java
@@ -0,0 +1,10 @@
+package com.junit;
+
+public class FiguryGeometryczne {
+    public double objętoscSzescianu(double a){
+
+        return a*a*a;
+    }
+
+
+}
--- GrupaD/Zajecia13/src/test/java/com/junit/AppTest.java
+++ GrupaD/Zajecia13/src/test/java/com/junit/AppTest.java
@@ -3,22 +3,33 @@
  */
 package com.junit;
 
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AppTest {
+    @Tag("przyklad")
     @Test
     public void przykladowyTest(){
         System.out.println("Przykladowy test");
     }
 
+    @Tag("math")
     @Test
     public void pierwszyTest(){
         App app = new App();
 
         System.out.println("porównuje liczby 25 i wynik funkcji");
-        assertEquals(25,app.dodaj(10,10));
+        assertEquals(20,app.dodaj(10,10));
+    }
+
+    @Tag("math")
+    @Test
+    public void testSzescianu(){
+        FiguryGeometryczne figuryGeometryczne = new FiguryGeometryczne();
+
+        assertEquals(125,figuryGeometryczne.objętoscSzescianu(5));
     }
 
 
