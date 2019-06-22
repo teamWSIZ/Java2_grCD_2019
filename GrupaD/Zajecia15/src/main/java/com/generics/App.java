530f3a47a6d9a1aa331f6087f3436b61afbf8c35
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2019-06-22 09:23:47 +0200

    cd..

--- GrupaD/Zajecia15/src/main/java/com/generics/App.java
+++ GrupaD/Zajecia15/src/main/java/com/generics/App.java
@@ -4,11 +4,8 @@
 package com.generics;
 
 public class App {
-    public String getGreeting() {
-        return "Hello world.";
-    }
 
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        
     }
 }
