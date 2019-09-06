c5ddffb1f491b4c93d6ae62f3841100f3966c48c
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2019-09-06 16:57:27 +0200

    cd..

--- GrupaC/Zajecia16/src/main/java/com/thread/App.java
+++ GrupaC/Zajecia16/src/main/java/com/thread/App.java
@@ -4,11 +4,19 @@
 package com.thread;
 
 public class App {
-    public String getGreeting() {
-        return "Hello world.";
+    static double mDane[] = new double[10];
+
+    static void dodajDane(){
+        for(int i=0;i<mDane.length;i++)
+            mDane[i] = i;
+    }
+
+    static void wyswietlDane(){
+
     }
 
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        dodajDane();
+
     }
 }
